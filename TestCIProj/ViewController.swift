9430ef3ad472d7a311c11b89234c1c5062ebe29a
yangfan_hailv <fan.yang@elestyle.jp>
2024-08-29 18:44:38 +0900

    nothing

---
+++ TestCIProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCIProj
+//
+//  Created by Hailv on 2024/8/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
